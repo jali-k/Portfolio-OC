759249cfc47cdb65d82e8862de8b4c17e005868d
Milinda Sachinthaka <e20286@eng.pdn.ac.lk>
2025-01-09 18:34:07 +0530

    test 3d model

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls } from "@react-three/drei"
+import Su35 from "../public/Su35"
+import "./App.css"
+
 
 function App() {
 
   return (
     <>
-      <h1>Test</h1>
+      <h1>Test Model</h1>
+      <Canvas style={{ width: '100vw', height: '100vh' }} className="canvas-container">
+      <OrbitControls />
+      <ambientLight intensity={1.5} />
+        <Suspense fallback={null}>
+          <Su35 />
+        </Suspense>
+      </Canvas>
     </>
   )
 }
